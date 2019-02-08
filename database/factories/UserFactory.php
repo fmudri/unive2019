1a32d2f7aae4d1f67a19cf61e771c66d903969d1
fmudri <filipmudri3@gmail.com>
2019-02-08 21:29:30 +0100

    8.2.2019

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,22 +3,29 @@
 use Faker\Generator as Faker;
 
 /*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Model Factories
+  |--------------------------------------------------------------------------
+  |
+  | This directory should contain each of the model factory definitions for
+  | your application. Factories provide a convenient way to generate new
+  | model instances for testing / seeding your application's database.
+  |
+ */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Rezervacija::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'predmet_id' => App\Predmet::select('id')->orderByRaw('RAND()')->first()->id,
+        'oznvrstadan' => $faker->randomElement([
+                'PO', 'UT', 'SR', 'CE', 'PE']),
+        'sat' => $faker->numberBetween(4, 20),
+        'created_at'=> now()
     ];
 });
+/**
+ *             $table->increments('id');
+            $table->integer('predmet_id')->unsigned();
+            $table->string('oznvrstadan', 2)->nullable();
+            $table->smallInteger('sat')->nullable();
+            $table->timestamps();
+ */
